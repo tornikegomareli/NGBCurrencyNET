767767aedacb94f8b9c9c8a531f5f2f256e67e6a
TornikeGomareli <gomarelidevelopment@gmail.com>
2020-04-04 18:13:58 +0400

    added currency client interface descsriptions

---
+++ NBGCurrency/Client/Interfaces/INGBCurrencyClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using NBGCurrency.Configuration;
+
+namespace NBGCurrency.Client.Interfaces
+{
+    public interface INGBCurrencyClient
+    {
+        Task<string> GetCurrencyAsync(CurrencyEnumCodes currencyEnumCode);
+        Task<string> GetCurrencyChangeAsync(CurrencyEnumCodes currencyEnumCode);
+        Task<string> GetCurrencyDescriptionAsync(CurrencyEnumCodes currencyEnumCode);
+        Task<string> GetCurrencyRateAsync(CurrencyEnumCodes currencyEnumCode);
+        Task<string> GetDate(CurrencyEnumCodes currencyEnumCode);
+    }
+}
--- NBGCurrency/Client/NGBCurrencyClient.cs
+++ NBGCurrency/Client/NGBCurrencyClient.cs
@@ -1,10 +1,58 @@
 ﻿using System;
+using System.Threading.Tasks;
+using NBGCurrency.Client.Interfaces;
+using NBGCurrency.Configuration;
+
 namespace NBGCurrency.Client
 {
-    public class NGBCurrencyClient
+    public class NGBCurrencyClient : INGBCurrencyClient
     {
-        public NGBCurrencyClient()
+        private static NGBCurrencyClient _instance;
+        private static volatile object _rootLock = new object();
+
+
+        public static NGBCurrencyClient Shared
         {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_rootLock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new NGBCurrencyClient();
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        public Task<string> GetCurrencyAsync(CurrencyEnumCodes currencyEnumCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetCurrencyChangeAsync(CurrencyEnumCodes currencyEnumCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetCurrencyDescriptionAsync(CurrencyEnumCodes currencyEnumCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetCurrencyRateAsync(CurrencyEnumCodes currencyEnumCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetDate(CurrencyEnumCodes currencyEnumCode)
+        {
+            throw new NotImplementedException();
         }
     }
 }
